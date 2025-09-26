b9698270c1a95840b7ea7c0811e6af69732ac899
lvppaul <levinhphat790@gmail.com>
2025-09-26 11:03:34 +0700

    fix: move swagger UI to root path for Render

--- PRN232.Lab1.CoffeeStore.APIS/Program.cs
+++ PRN232.Lab1.CoffeeStore.APIS/Program.cs
@@ -51,9 +51,10 @@ namespace PRN232.Lab1.CoffeeStore.APIS
                 app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "CoffeeStore API v1");
-                    c.RoutePrefix = "swagger"; // cho phép truy cập tại /swagger
+                    c.RoutePrefix = string.Empty; // mở Swagger ở root "/"
                 });
             }
+
 
             // Nếu muốn chỉ bật HTTPS khi dev
             if (app.Environment.IsDevelopment())
